becaa7f9074cd68bc19ffb07e3ef4728658fbf29
Fox <Fox.mary@rambler.ru>
2021-11-08 14:03:04 +0300

    Task-2 Переписала с 0.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,39 +1,20 @@
+import lombok.Data;
+
+@Data
 public class Task {
-    private int id;
+    private String description;
     private boolean status;
-    private final String description;
     private String statusIcon;
 
-    public String getStatusIcon() {
-        return statusIcon;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public boolean isStatus() {
-        return status;
+    public Task(String description) {
+        this.description = description;
+        status = false;
+        statusIcon = "[ ]";
     }
 
     public void setStatus(boolean status) {
         this.status = status;
         if (status) statusIcon = "[x]";
         else statusIcon = "[ ]";
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Task(int id, String description) {
-        this.id = id;
-        this.status = false;
-        this.description = description;
-        statusIcon = "[ ]";
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,109 +1,103 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static java.lang.System.*;
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class TaskList {
-    private final List<Task> taskList = new ArrayList<>();
+    private final HashMap<Integer, Task> taskList = new HashMap<>();
+    int count = 0;
 
     public void commands() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        final Scanner scanner = new Scanner(in);
 
-        while (true) {
-            String msg = reader.readLine();
+        while (scanner.hasNext()) {
+            String msg = scanner.next();
+            if (msg.equals("add")) add(scanner);
+            else if (msg.equals("edit")) edit(scanner);
+            else if (msg.equals("delete")) delete(scanner);
+            else if (msg.equals("print")) print(scanner);
+            else if (msg.equals("toggle")) toggle(scanner);
+            else if (msg.equals("search")) search(scanner);
+            else if (msg.equals("quit")) return;
+            else out.println("Неверная команда");
+        }
+    }
+
+    private void add(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        if (checkArgument(msg)) {
+            count += 1;
+            taskList.put(count, new Task(msg));
+        }
+    }
+
+    private void edit(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        if (checkArgument(msg)) {
             String[] tokens = msg.split("\\s", 2);
-            if (msg.startsWith("add")) {
+            int id = checkId(tokens[0]);
                 if (tokens.length == 1) out.println("Неверное описание задачи");
-                else add(tokens[1]);
-            }else if (msg.startsWith("edit")) {
-                if (tokens.length == 1) out.println("Неверные номер и описание задачи");
-                else edit(tokens[1]);
-            }else if (msg.startsWith("delete")) {
-                if (tokens.length == 1) out.println("Неверный номер задачи");
-                else delete(tokens[1]);
-            } else if (msg.startsWith("print")) {
-                if (tokens.length == 1) print(false);
-                else if (tokens[1].equals("all")) print(true);
-                else out.println("Неверный аргумент");
-            } else if (msg.startsWith("toggle")) {
-                if (tokens.length == 1) out.println("Неверный номер задачи");
-                else toggle(tokens[1]);
-            } else if (msg.startsWith("search")) {
-                if (tokens.length == 1) out.println("Неверный критерий поиска");
-                else search(tokens[1]);
-            }
-            else if (msg.startsWith("quit")) {
-                reader.close();
-                break;
-            } else out.println("Неверная команда");
+                else if (id != -1 & taskList.containsKey(id)) taskList.get(id).setDescription(tokens[1]);
+                else out.println("Задачи с таким номером не найдено");
         }
     }
 
-    private void add(String description) {
-        taskList.add(new Task(taskList.size() + 1, description));
-    }
-
-    private void edit(String msg) {
-        String[] tokens = msg.split("\\s", 2);
-        if (tokens.length == 1) out.println("Неверное описание задачи");
-        else {
-            try {
-                int i = Integer.parseInt(tokens[0]);
-                if (i <= 0 || i-1 >= taskList.size()) out.println("Задача с таким номером не найдена");
-                else taskList.set(i-1, new Task(i, tokens[1]));
-            } catch (NumberFormatException e) {
-                out.println("Неверный номер задачи");
-            }
+    private void delete(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        if (checkArgument(msg)) {
+            int id = checkId(msg);
+                if (id != -1) taskList.remove(id);
         }
     }
 
-    private void delete(String id) {
+    private void print(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        for (int i = 1; i <= count; i++) simplePrint(i, msg.equals("all") || !taskList.get(i).isStatus());
+    }
+
+    private void search(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        if (checkArgument(msg)) {
+            for (int i = 1; i <= count; i++) simplePrint(i, taskList.get(i).getDescription().contains(msg));
+        }
+    }
+
+    private void simplePrint(int i, boolean b) {
+        boolean flag = true;
+        if (taskList.containsKey(i)) {
+            if (b) {
+                out.println(i + ". " + taskList.get(i).getStatusIcon() + " " + taskList.get(i).getDescription());
+                flag = false;
+            }
+        }
+        if (flag) out.println("Задач не найдено");
+    }
+
+    private void toggle(Scanner scanner) {
+        String msg = scanner.nextLine().trim();
+        if (checkArgument(msg)) {
+            int id = checkId(msg);
+            if (taskList.containsKey(id)) taskList.get(id).setStatus(!taskList.get(id).isStatus());
+            else if (id != -1) out.println("Задачи с таким номером не найдено");
+        }
+    }
+
+    private boolean checkArgument(String msg) {
+        if (msg.length() == 0) {
+            out.println("Нехватает аргумента");
+            return false;
+        } else return true;
+    }
+
+    private static int checkId(String stringId) {
+        int id = -1;
         try {
-            int i = Integer.parseInt(id);
-            if (i <= 0 || i-1 >= taskList.size()) out.println("Задача с таким номером не найдена");
-            else taskList.remove(i-1);
-
-            for (Task t : taskList) {
-                if (t.getId() == i + 1) {
-                    t.setId(i);
-                    i++;
-                }
-            }
+            id = Integer.parseInt(stringId);
+            return id;
         } catch (NumberFormatException e) {
             out.println("Неверный номер задачи");
         }
-    }
-
-    private void search(String msg) {
-        boolean coincidence = false;
-        for (Task t : taskList) {
-            if (t.getDescription().contains(msg)) {
-                coincidence = true;
-           out.println(t.getId() + ". " + t.getStatusIcon() + " " + t.getDescription());
-            }
-        }
-        if (!coincidence) out.println("Задач не найдено");
-    }
-
-    private void print(boolean argument) {
-        for (Task t : taskList) {
-            if (argument || !t.isStatus()) out.println(t.getId() + ". " + t.getStatusIcon() + " " + t.getDescription());
-        }
-    }
-
-    private void toggle(String id) {
-        try {
-            int i = Integer.parseInt(id);
-            if (i > 0 & i < taskList.size()) {
-                for (Task t : taskList) {
-                    if (t.getId() == i) t.setStatus(!t.isStatus());
-                }
-            } else out.println("Задача с таким номером не найдена");
-        } catch (NumberFormatException e) {
-            out.println("Неверный номер задачи");
-        }
+        return id;
     }
 }
